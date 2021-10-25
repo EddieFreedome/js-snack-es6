4e33dd674f5caccdf507df6c63e717092f7a42d8
EddieFreedome <eddie.freed@tutanota.com>
2021-10-25 16:47:30 +0200

    completato esercizio

--- snack-1-bici/script.js
+++ snack-1-bici/script.js
@@ -8,28 +8,62 @@ const arrBici = [
     
     {
         nome: "Bianchi Methanol",
-        peso: `${12.5}Kg`,
-        immagine: "img/bianchi-methanol.webp"
+        peso: 12.5,
+        immagine: "img/bianchi-methanol.webp",
+        descrizione: "La bici davvero piu' leggera"
     },
 
     {
         nome: "Rockrider XC 940",
-        peso: `${12}Kg`,
-        immagine: "img/bianchi-methanol.webp"
+        peso: 12,
+        immagine: "img/rockrider-xc940.jpg",
+        descrizione: "La bici davvero piu' leggera"
     },
 
     {
         nome: "Trek XCaliber 8 ",
-        peso: `${11.5}Kg`,
-        immagine: "img/bianchi-methanol.webp"
+        peso: 11.5,
+        immagine: "img/trek-x-caliber-8.jpg",
+        descrizione: "La bici davvero piu' leggera"
     },
 
 ]
+
+let biciLeggera = arrBici[0]
 
 console.log(arrBici[0], arrBici[1], arrBici[2]);
 
 //creo una costante peso per ogni oggetto dell'array
 for (let i = 0; i < arrBici.length; i++) {
-    let {peso} = arrBici[i];
-    
+    if (biciLeggera.peso > arrBici[i].peso){
+        biciLeggera = arrBici[i]
+    }
 }
+console.log(biciLeggera);
+
+const {nome, peso} = biciLeggera
+const cardContainer = document.querySelector(".main-container")
+
+cardContainer.innerHTML = ""
+
+generaCard()
+function generaCard(){
+    const content = ` 
+    <div class="cycle-card">
+        <div class="card-image">
+            <img
+                src="${biciLeggera.immagine}"
+                alt=""
+            />
+        </div>
+        <div class="card-text">
+            <h3>${biciLeggera.nome}</h3>
+            <p>${biciLeggera.descrizione}</p>
+        </div>
+    </div>
+`;
+
+cardContainer.innerHTML += content;
+   
+}
+
