c18c257e341dc338067e8d8e68ce3b66863fed6e
EddieFreedome <eddie.freed@tutanota.com>
2021-10-25 17:35:20 +0200

    continued

--- snack-2-calcio/script.js
+++ snack-2-calcio/script.js
@@ -19,8 +19,18 @@ const arrSquadre = [
     },
 ]
 
-function generateRandomNum(minNumber = 1, maxNumber = 10) {
-    const numRandom = Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
+let puntiFatti = arrSquadre.punti_fatti
+let falliSubiti = arrSquadre.falli_subiti
+console.log(puntiFatti)
+
+// function generateRandomNum(minNumber = 1, maxNumber = 10) {
+//     const numRandom = Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
   
-    return numRandom;
-  }
+//     return numRandom;
+// }
+
+// for (let i = 0; i < arrSquadre.length; i++) {
+//     arrSquadre[i].punti_fatti = generateRandomNum(arrSquadre[i].punti_fatti);
+//     arrSquadre[i].falli_subiti = generateRandomNum(arrSquadre[i].falli_subiti);
+// }
+// console.log(punti_fatti, falli_subiti);
